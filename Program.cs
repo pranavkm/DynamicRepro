cde782abe09dbe4828eac504318cb53e784a639a
Pranav K <prkrishn@hotmail.com>
2021-09-23 11:17:41 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp23;
+using System.Dynamic;
+using System.Linq.Expressions;
+
+Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    var container = new DynamicContainer();
+    Repro.RunController(container);
+    Repro.RunView(container);
+
+    Console.ReadLine();
+
+}
+
+
---
+++ Repro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Repro
+    {
+        public static void RunController(DynamicContainer container)
+        {
+            container.Dynamic.Test = "Key";
+        }
+
+        public static void RunView(DynamicContainer container)
+        {
+            Console.WriteLine(container.Dynamic.Test);
+        }
+    }
+
+    class DynamicContainer
+    {
+        public dynamic Dynamic { get; } = new DynamicDictionary();
+    }
+
+    class DynamicDictionary : DynamicObject
+    {
+        private readonly Dictionary<string, object?> _dictionary = new();
+
+        public override bool TryGetMember(GetMemberBinder binder, out object? result)
+        {
+            result = _dictionary[binder.Name];
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object? value)
+        {
+            _dictionary[binder.Name] = value;
+            return true;
+        }
+
+    }
+}
